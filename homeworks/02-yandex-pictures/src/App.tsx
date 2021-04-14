15a0067ef1da3881ba0ab6c54ff5ced6a07a8b31
mikhail.nakanechny <mikhail.nakanechny@ewave.com>
2021-04-14 18:33:26 +0300

    02-feature: added fetch imajes json

--- homeworks/02-yandex-pictures/src/App.tsx
+++ homeworks/02-yandex-pictures/src/App.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import './App.css';
 
+// https://dog.ceo/api/breed/corgi-cardigan/images
+// https://images.dog.ceo/breeds/corgi-cardigan
+
+fetch("https://dog.ceo/api/breed/corgi/cardigan/images")
+  .then(response => {
+    console.log(response);
+    // console.log(response.json());
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Yandex-like layout
-        </p>        
+        </p>
       </header>
+      <main>
+        <div className="demo-image">
+          <img src="https://images.dog.ceo/breeds/corgi-cardigan/n02113186_8794.jpg"/>            
+        </div>
+      </main>
     </div>
   );
 }
