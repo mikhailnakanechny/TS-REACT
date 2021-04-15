10c2f052d14d8ad7a277a3f7044c430ad502abbc
mikhail.nakanechny <mikhail.nakanechny@ewave.com>
2021-04-15 22:15:34 +0300

    02-fix: fixed images proportion loss

--- homeworks/02-yandex-pictures/src/App.tsx
+++ homeworks/02-yandex-pictures/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
+        <h1 className="header-title">
           Yandex-like layout
         </h1>
       </header>
