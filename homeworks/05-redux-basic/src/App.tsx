c347fbb75857fe77fc44ba4f242c19344e899ba1
Mikhail Nakanechny <mikhailnakanechny@gmail.com>
2021-05-02 22:37:41 +0300

    05-feature: added react and types, added balance function and store, added test dispatching

---
+++ homeworks/05-redux-basic/src/App.tsx
@@ -0,0 +1,30 @@
+import './App.css';
+import {store} from './redux/balance' 
+
+//Test array
+const array = [
+  { type: 'UPDATE_BALANCE', payload: 1000.0 },
+  { type: 'CREDIT', payload: 200.0 },
+  { type: 'CREDIT', payload: 100.0 },
+  { type: 'SET_BALANCE_WITH_TAX', payload: 14.0 },
+  { type: 'DEBIT', payload: 250.0 },
+  { type: 'UPDATE_BALANCE', payload: 1000.0 },
+];
+
+//Test actions dispatching
+array.forEach(action => {
+  console.log(action)
+  store.dispatch(action)
+});
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        05-redux-basic
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ homeworks/05-redux-basic/src/redux/balance.ts
@@ -0,0 +1,29 @@
+import { createStore, compose } from 'redux';
+
+//Devtools config
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// balance function and store 
+export type IBalanceAction = {type: string, payload: number}
+
+const actionTypeManager = (state: number = 0, action: IBalanceAction) => {
+    switch (action.type) {
+        case 'UPDATE_BALANCE':
+            return action.payload;
+        case 'SET_BALANCE_WITH_TAX':
+            return state - state * action.payload / 100;
+        case 'CREDIT':
+            return state - action.payload;
+        case 'DEBIT':
+            return state + action.payload;
+        default:
+            return state;
+    }
+};
+
+export const store = createStore(actionTypeManager, composeEnhancers());
